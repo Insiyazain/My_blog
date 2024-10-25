b2e22ffcef76b016ddfcd8c6426b39acebdd91a0
insiyazain <insiyazain123@gmail.com>
2024-10-25 09:33:10 +0500

    making first_page of blog

--- mypage/blog/urls.py
+++ mypage/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts",views.posts, name="post-page"),
-    path("posts/<slug>",views.post_details, name="post-detail-page")#posts/my_first_post
+    path("posts/<slug:slug>",views.post_details, name="post-detail-page")#posts/my_first_post
 ]
